843390067552cdfa875f04c212936dd9f144889f
pchp250g8 <pchp250g8@tutamail.com>
2025-12-10 16:09:05 +0300

    Добавьте файлы проекта.

---
+++ CalcGcd/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("CalcGcd")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CalcGcd")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("4a64bdb7-9e5e-4086-ab20-c005fd69f775")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
